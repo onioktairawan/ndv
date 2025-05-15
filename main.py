130d99b867afb77feb99a0cec516b6830e1b88b8
serpa <122547358+onioktairawan@users.noreply.github.com>
2025-05-16 03:10:43 +0700

    Update main.py

--- main.py
+++ main.py
@@ -1,13 +1,15 @@
+# main.py
 from pyrogram import Client
 from config import API_ID, API_HASH, BOT_TOKEN
+
+app = Client("bot", api_id=API_ID, api_hash=API_HASH, bot_token=BOT_TOKEN)
+
+# Import handler-setelah app didefinisikan agar handler bisa pakai app
 import handlers.start_handler
 import handlers.auth_handler
 import handlers.groups_handler
 import handlers.callback_handler
 
-app = Client("bot", api_id=API_ID, api_hash=API_HASH, bot_token=BOT_TOKEN)
-
 if __name__ == "__main__":
     print("Bot berjalan...")
     app.run()
-
